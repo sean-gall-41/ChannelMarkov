eb394cf352f91155ebc9b571b88fb3c5067a970f
Sean Gallogly <galloglyst149@gmail.com>
2023-03-13 21:04:30 -0500

    added state transitions and finished run method

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,72 @@ impl Channel {
             state: in_state
         }
     }
+
+    fn make_transition(&mut self) {
+        let choice = rand::thread_rng().gen_range(0..2);
+        match self.state {
+            ChannelState::Closed1 => {
+                if rand::thread_rng().gen::<f32>() < 4.0 * OPEN_RATE {
+                    self.state = ChannelState::Closed2;
+                    return;
+                }
+            },
+            ChannelState::Closed2 => {
+                match choice {
+                    0 => {
+                        if rand::thread_rng().gen::<f32>() < CLOSE_RATE {
+                            self.state = ChannelState::Closed1;
+                        }
+                    },
+                    1 => {
+                        if rand::thread_rng().gen::<f32>() < 3.0 * OPEN_RATE {
+                            self.state = ChannelState::Closed3;
+                        }
+                    },
+                    _ => (),
+                }
+                return;
+            },
+            ChannelState::Closed3 => {
+                match choice {
+                    0 => {
+                        if rand::thread_rng().gen::<f32>() < 2.0 * CLOSE_RATE {
+                            self.state = ChannelState::Closed2;
+                        }
+                    },
+                    1 => {
+                        if rand::thread_rng().gen::<f32>() < 2.0 * OPEN_RATE {
+                            self.state = ChannelState::Closed4;
+                        }
+                    },
+                    _ => (),
+                }
+                return;
+            },
+            ChannelState::Closed4 => {
+                match choice {
+                    0 => {
+                        if rand::thread_rng().gen::<f32>() < 3.0 * CLOSE_RATE {
+                            self.state = ChannelState::Closed3;
+                        }
+                    },
+                    1 => {
+                        if rand::thread_rng().gen::<f32>() < OPEN_RATE {
+                            self.state = ChannelState::Open;
+                        }
+                    },
+                    _ => (),
+                }
+                return;
+            },
+            ChannelState::Open => {
+                if rand::thread_rng().gen::<f32>() < 4.0 * CLOSE_RATE {
+                    self.state = ChannelState::Closed4;
+                    return;
+                }
+            },
+        }
+    }
 }
 
 pub struct Simulation<'a> {
@@ -94,14 +160,28 @@ impl Simulation<'static> {
         }
     }
 
-    fn run(&self) {
-        todo!["pls impl me :-)"];
+    fn from(num_channels: u32, num_ts: u32) -> Self {
+        Simulation {
+            num_channels: num_channels,
+            num_ts: num_ts,
+            channels: vec![Channel::new(true); num_channels as usize],
+            t_mat: &TRANSITION_MATRIX,
+        }
+    }
+
+    fn run(&mut self) {
+        for _ in 0..self.num_ts {
+            for channel in &mut self.channels {
+                channel.make_transition();
+                //println!("{:?}", channel.state);
+            }
+        }
     }
 }
 
 fn main() -> Result<(), Error> {
-    let model_sim = Simulation::new();
-    println!("Hello, world!");
+    let mut model_sim = Simulation::from(1_000, 1_000);
+    model_sim.run();
     Ok(())
 }
 
