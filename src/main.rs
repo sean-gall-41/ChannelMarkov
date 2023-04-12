2da89e194009a2fb0490f9be84f784946d5e32af
Sean Gallogly <galloglyst149@gmail.com>
2023-04-12 13:59:28 -0500

    temp commit: initialized HMMNaSim, need to test

--- src/main.rs
+++ src/main.rs
@@ -593,52 +593,80 @@ impl HMMNaSim {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // initial values for delayed K+ rectifier conductance
     let params_json = json::parse(fs::read_to_string(PARAM_FILE_NAME)?.as_str()).unwrap();
-    let k_model_params = &params_json["potassium"];
-    let mut model_hmm_sim = HMMSim::from(k_model_params["num_channels"].as_u32().unwrap(),
-                                         k_model_params["total_time"].as_u32().unwrap(),
-                                         k_model_params["dt"].as_f32().unwrap(),
-                                         ChannelParams::from(
-                                            k_model_params["e_reverse"].as_f32().unwrap(),
-                                            k_model_params["init_open_rate"].as_f32().unwrap()
-                                            * k_model_params["dt"].as_f32().unwrap(),
-                                            k_model_params["open_exp_const"].as_f32().unwrap(),
-                                            k_model_params["init_close_rate"].as_f32().unwrap()
-                                            * k_model_params["dt"].as_f32().unwrap(),
-                                            k_model_params["close_exp_const"].as_f32().unwrap(),
-                                        ),
-                                         &k_model_params["stimulus"],
-                                         &k_model_params["emissions"]);
+    //let k_model_params = &params_json["potassium"];
+    //let mut model_hmm_sim = HMMSim::from(k_model_params["num_channels"].as_u32().unwrap(),
+    //                                     k_model_params["total_time"].as_u32().unwrap(),
+    //                                     k_model_params["dt"].as_f32().unwrap(),
+    //                                     ChannelParams::from(
+    //                                        k_model_params["e_reverse"].as_f32().unwrap(),
+    //                                        k_model_params["init_open_rate"].as_f32().unwrap()
+    //                                        * k_model_params["dt"].as_f32().unwrap(),
+    //                                        k_model_params["open_exp_const"].as_f32().unwrap(),
+    //                                        k_model_params["init_close_rate"].as_f32().unwrap()
+    //                                        * k_model_params["dt"].as_f32().unwrap(),
+    //                                        k_model_params["close_exp_const"].as_f32().unwrap(),
+    //                                    ),
+    //                                     &k_model_params["stimulus"],
+    //                                     &k_model_params["emissions"]);
 
-    model_hmm_sim.run();
+    //model_hmm_sim.run();
 
-    let mut model_hhk_sim = HHKSim::from(k_model_params["total_time"].as_u32().unwrap(),
-                                         k_model_params["dt"].as_f32().unwrap(),
-                                         k_model_params["emissions"][4]["mu"].as_f32().unwrap(),
-                                         ChannelParams::from(
-                                            k_model_params["e_reverse"].as_f32().unwrap(),
-                                            k_model_params["init_open_rate"].as_f32().unwrap()
-                                            * k_model_params["dt"].as_f32().unwrap(),
-                                            k_model_params["open_exp_const"].as_f32().unwrap(),
-                                            k_model_params["init_close_rate"].as_f32().unwrap()
-                                            * k_model_params["dt"].as_f32().unwrap(),
-                                            k_model_params["close_exp_const"].as_f32().unwrap(),
-                                        ),
-                                         &k_model_params["stimulus"]);
+    //let mut model_hhk_sim = HHKSim::from(k_model_params["total_time"].as_u32().unwrap(),
+    //                                     k_model_params["dt"].as_f32().unwrap(),
+    //                                     k_model_params["emissions"][4]["mu"].as_f32().unwrap(),
+    //                                     ChannelParams::from(
+    //                                        k_model_params["e_reverse"].as_f32().unwrap(),
+    //                                        k_model_params["init_open_rate"].as_f32().unwrap()
+    //                                        * k_model_params["dt"].as_f32().unwrap(),
+    //                                        k_model_params["open_exp_const"].as_f32().unwrap(),
+    //                                        k_model_params["init_close_rate"].as_f32().unwrap()
+    //                                        * k_model_params["dt"].as_f32().unwrap(),
+    //                                        k_model_params["close_exp_const"].as_f32().unwrap(),
+    //                                     ),
+    //                                     &k_model_params["stimulus"]);
 
-    model_hhk_sim.run();
+    //model_hhk_sim.run();
+
+    let na_model_params = &params_json["sodium"];
+    let mut model_nahmm_sim = HMMNaSim::from(na_model_params["num_channels"].as_u32().unwrap(),
+                                           na_model_params["total_time"].as_u32().unwrap(),
+                                           na_model_params["dt"].as_f32().unwrap(),
+                                           NaChannelParams::from(
+                                              na_model_params["k_1"].as_f32().unwrap(),
+                                              na_model_params["k_2"].as_f32().unwrap(),
+                                              na_model_params["k_3"].as_f32().unwrap(),
+                                              na_model_params["m_pre_v_fact_open"].as_f32().unwrap()
+                                              * na_model_params["dt"].as_f32().unwrap(),
+                                              na_model_params["m_pre_v_fact_open"].as_f32().unwrap(),
+                                              na_model_params["m_v_offset_open"].as_f32().unwrap(),
+                                              na_model_params["init_m_close_rate"].as_f32().unwrap()
+                                              * na_model_params["dt"].as_f32().unwrap(),
+                                              na_model_params["close_m_exp_const"].as_f32().unwrap(),
+                                              na_model_params["m_v_offset_close"].as_f32().unwrap(),
+                                              na_model_params["init_h_open_rate"].as_f32().unwrap()
+                                              * na_model_params["dt"].as_f32().unwrap(),
+                                              na_model_params["h_v_offset_open"].as_f32().unwrap(),
+                                              na_model_params["open_h_exp_const"].as_f32().unwrap(),
+                                              na_model_params["h_pre_v_fact_close"].as_f32().unwrap(),
+                                              na_model_params["h_pre_v_fact_close"].as_f32().unwrap(),
+                                              na_model_params["h_v_offset_close"].as_f32().unwrap(),
+                                              na_model_params["e_reverse"].as_f32().unwrap()
+                                           ),
+                                           &na_model_params["stimulus"],
+                                           &na_model_params["emissions"]);
 
     // it's plotting time
     let root = BitMapBackend::new(OUT_IMG_FILE_NAME, (1024, 600)).into_drawing_area();
     root.fill(&WHITE)?;
-    let root = root.titled(format!("HMM vs HH emissions num_channels={}", model_hmm_sim.num_channels).as_str(),
+    let root = root.titled(format!("HMM vs HH emissions num_channels={}", model_nahmm_sim.num_channels).as_str(),
                            ("sans-serif", 40))?;
 
     let mut chart = ChartBuilder::on(&root)
         .margin(10)
         .set_label_area_size(LabelAreaPosition::Left, 45)
         .set_label_area_size(LabelAreaPosition::Bottom, 40)
-        .build_cartesian_2d(-50f32..((model_hmm_sim.total_time as f32) / model_hmm_sim.dt + 50f32),
-                                                (-15.0 * model_hmm_sim.num_channels as f32)..(40.0 * model_hmm_sim.num_channels as f32))?;
+        .build_cartesian_2d(-50f32..((model_nahmm_sim.total_time as f32) / model_nahmm_sim.dt + 50f32),
+                                                (-15.0 * model_nahmm_sim.num_channels as f32)..(40.0 * model_nahmm_sim.num_channels as f32))?;
 
     chart
         .configure_mesh()
@@ -650,17 +678,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .draw()?;
 
     chart.draw_series(LineSeries::new(
-        model_hmm_sim.cum_emis.iter().enumerate().map(|(ts, e)| {
+        model_nahmm_sim.cum_emis.iter().enumerate().map(|(ts, e)| {
             (ts as f32, *e)
         }),
         &BLUE,
     ))?;
-    chart.draw_series(LineSeries::new(
-        model_hhk_sim.emis_hist.iter().enumerate().map(|(ts, e)| {
-            (ts as f32, model_hmm_sim.num_channels as f32 * *e)
-        }),
-        &BLACK,
-    ))?;
+    //chart.draw_series(LineSeries::new(
+    //    model_hhk_sim.emis_hist.iter().enumerate().map(|(ts, e)| {
+    //        (ts as f32, model_hmm_sim.num_channels as f32 * *e)
+    //    }),
+    //    &BLACK,
+    //))?;
 
     // uncomment for individual channel emission time series
     //for channel in model_sim.channels {
